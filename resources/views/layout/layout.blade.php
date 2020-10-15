e40dde0fb500e3c9c03a0135411060380ce35136
minhduc19669 <phongnhiryan@gmail.com>
2020-10-15 17:00:45 +0700

    ok

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -195,9 +195,7 @@
                 </div>
               </div>
             </div>
-            <div
-              class="mobile-menu-area electro-menu d-md-block col-md-12 col-lg-12 col-12 d-lg-none d-xl-none"
-            >
+            <div class="mobile-menu-area electro-menu d-md-block col-md-12 col-lg-12 col-12 d-lg-none d-xl-none">
               <div class="mobile-menu">
                 <nav id="mobile-menu-active">
                   <ul class="menu-overflow">
@@ -579,78 +577,41 @@
     <script src="{{asset('assets_page/js/owl.carousel.min.js')}}"></script>
     <script src="{{asset('assets_page/js/plugins.js')}}"></script>
     <script src="{{asset('assets_page/js/main.js')}}"></script>
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-        <script src="{{asset('assets_page/js/cart.js')}}"></script>
-        <script>
-            $(document).ready(function($){
-                $('.ship-wrapper .single-ship .radio-check').on('click',function(){
-                    $('.ship-wrapper .single-ship .radio-check').not(this).prop('checked',false);
-                });
-                $("select[name='city']").change(function () {
-                    let city_id = $(this).val();
-                    let origin = location.origin;
-                    $.ajaxSetup({
-                        headers: {
-                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+    <script src="{{asset('assets_page/js/cart.js')}}"></script>
+	<script>
+let listProduct2=$("#product5 #addtocart5");
+                listProduct2.on('click',function(){
+                    var id=$(this).attr('buy-id1');
+                    $.ajax({
+                    url:'http://laravel-training.local/cart/add/'+id,
+                    dataType:'json',
+                    success:function(result){
+                        let count = result.countCart;
+                        let data=result.contentCart;
+                        let element="";
+                        $.each(data,function(key,value){
+                        element+= '<li class="single-shopping-cart" id="'+'item_id_'+value.rowId+ '">'+'<div class="shopping-cart-img">' +'<a href="#" >'+'<img src="' +'../product'+'/'+ value.options.image + '" />'+'</a>'+'</div>'+'<div class="shopping-cart-title">'+'<h4>'+'<a href="">'+value.name+'</a>'+'</h4>'+'<h6>Số lượng: '+value.qty+'</h6>'+'<span>Giá: '+value.price+'</span>'+'</div>'+'<div id="productcart" class="shopping-cart-delete" style="">'+'<a style="cursor: pointer;" id="deleteitem" item-id="'+value.rowId+'">'+'<i class="ti-close">'+'</i>'+'</a>'+'</div>'+'</li>';
+                        });
+                        $('#cart').html(element);
+                        $("#countcart").html(""+count);
+                        $("#total").html(result.total+" VNĐ")
                         }
                     });
-                    $.ajax({
-                        url:origin+'/cart/district/'+city_id,
-                        data: {},
-                        dataType: 'json',
-                        success:function(data){
-                            console.log(data);
-                            $("select[name='district']").children().remove();
-                            $("select[name='district']").focus();
-                            $("select[name='district']").append(
-                                "<option value=''>" + "--Quận/Huyện--" + "</option>"
-                            );
-                            // console.log(result);
-                            $.each(data, function (key, value) {
-                                $("select[name='district']").append(
-                                    "<option value=" + value.id + ">" + value.name + "</option>"
-                                );
-                            })
-                        }
-                    })
-
-
-
-                });
-                $("select[name='city_receive']").change(function () {
-                    let city_id = $(this).val();
-                    let origin = location.origin;
-                    $.ajaxSetup({
-                        headers: {
-                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                        }
+                    Swal.fire({
+                        title: 'Sản phẩm đã được thêm vào giỏ hàng của bạn!',
+                        icon:'success',
+                        background: '#fff url(/images/trees.png)',
+                        backdrop: `
+                            rgba(0,0,123,0.4)
+                            url("/images/nyan-cat.gif")
+                            left top
+                            no-repeat
+                        `
                     });
-                    $.ajax({
-                        url:origin+'/cart/district/'+city_id,
-                        data: {},
-                        dataType: 'json',
-                        success:function(data){
-                            console.log(data);
-                            $("select[name='district_receive']").children().remove();
-                            $("select[name='district_receive']").focus();
-                            $("select[name='district_receive']").append(
-                                "<option value=''>" + "--Quận/Huyện--" + "</option>"
-                            );
-                            // console.log(result);
-                            $.each(data, function (key, value) {
-                                $("select[name='district_receive']").append(
-                                    "<option value=" + value.id + ">" + value.name + "</option>"
-                                );
-                            })
-                        }
-                    })
-
-
-
-                })
-        });
-
-        </script>
+                });
+    </script>
 </body>
 </html>
